8510f3253e834918eae4d6cdd15e17e3e45186bf
Flavio Andrade <foa12381@gmail.com>
2023-01-05 13:06:42 -0300

    pages/routes

--- perfil/src/App.js
+++ perfil/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Contact from './pages/contact';
+import Perfil from './pages/perfil';
+import Project from './pages/project';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route
+          path="/"
+          element={<Perfil/>}
+        />
+        <Route
+          path="/project"
+          element={<Project/>}
+        />
+        <Route
+          path="/contact"
+          element={<Contact/>}
+          ></Route>
+      </Routes>
     </div>
   );
 }
---
+++ perfil/src/pages/perfil.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Perfil() {
+  return (
+    <div>perfil</div>
+  )
+}
